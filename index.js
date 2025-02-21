a81aeccdbd84c7cac7d37bdfef13adc5de83e22f
DHINESHMOHAN17 <dhineshdevi18@gmail.com>
2025-02-22 01:19:06 +0530

    formatting

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
-let calculation=localStorage.getItem('calculation') || "";
-let displayElement=document.querySelector(".display input");
-function displaying(num){
-    calculation+=num;
+let calculation = localStorage.getItem("calculation") || "";
+let displayElement = document.querySelector(".display input");
+function displaying(num) {
+  calculation += num;
+  ondisplay();
+}
+function clearTheCalcalution() {
+  calculation = "";
+  ondisplay();
+}
+function deleted() {
+  calculation = calculation.slice(0, -1);
+  ondisplay();
+}
+function result() {
+  try {
+    calculation = eval(calculation);
     ondisplay();
-
+  } catch (err) {
+    alert("Invalid Calculation");
+  }
 }
-function clearTheCalcalution(){
-    calculation="";
-    ondisplay();
-
-}
-function deleted(){
-    calculation=calculation.slice(0,-1);
-    ondisplay();
-
-}
-function result(){
-    try{
-    calculation=eval(calculation);
-    ondisplay();}
-    catch(err){
-        alert("Invalid Calculation");
-    }
-
-}
-function ondisplay(){
-    displayElement.value=calculation;
-    localStorage.setItem('calculation',calculation);
+function ondisplay() {
+  displayElement.value = calculation;
+  localStorage.setItem("calculation", calculation);
 }
